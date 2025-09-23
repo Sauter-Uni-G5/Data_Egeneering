e3b8bf7d0a27f6cf3cda48bb04f469506f714d30
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-09-23 14:24:51 -0300

    Implementado endpoint principal para executar pipelina completa da engenharia de dados.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.controllers import ear_controller, hydro_controller, upload_controller, weather_controller, registry_controller
+from app.controllers import pipeline_controller
 
 app = FastAPI(title="Sauter University Data Engineering API")
 
@@ -8,6 +9,7 @@ app.include_router(hydro_controller.router, prefix="/api")
 app.include_router(ear_controller.router, prefix="/api")
 app.include_router(registry_controller.router, prefix="/api")
 app.include_router(upload_controller.router, prefix="/api")
+app.include_router(pipeline_controller.router, prefix="/api")
 
 @app.get("/")
 def read_root():
