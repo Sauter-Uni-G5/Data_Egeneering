3564c1f76d32da4b52181238511f7edc7170aab2
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2025-09-24 14:05:23 -0300

    Removido tudo relacionado a weather!

--- app/pipeline/tests/data_cleaner_test.py
+++ app/pipeline/tests/data_cleaner_test.py
@@ -2,13 +2,9 @@ import pandas as pd
 from app.controllers.registry_controller import get_registry_direct
 from app.controllers.hydro_controller import get_hydro_data_direct
 from app.controllers.ear_controller import get_ear_data_direct
-from app.controllers.weather_controller import get_weather_direct
 
 from app.pipeline.extractors.ons_extractor import (
     extract_registry_to_df, extract_hydro_to_df, extract_ear_to_df
-)
-from app.pipeline.extractors.weather_extractor import (
-    extract_weather_to_dict, extract_multiple_weather_to_df
 )
 from app.pipeline.transformers.data_cleaner import clean_and_normalize
 
@@ -36,15 +32,6 @@ def main():
     df_ear_clean = clean_and_normalize(df_ear, date_col="ear_data" if "ear_data" in df_ear.columns else None)
     df_ear_clean.to_csv("ear_cleaned.csv", index=False, encoding="utf-8")
     print("Tabela ear_cleaned.csv gerada.")
-
-    # 4. Weather
-    raw_weather = get_weather_direct(-15.7939, -47.8828, "2020-01-01", "2020-01-03")
-    weather_dict = extract_weather_to_dict(raw_weather, -15.7939, -47.8828, "BSB")
-    weather_list = [weather_dict]
-    df_weather = extract_multiple_weather_to_df(weather_list)
-    df_weather_clean = clean_and_normalize(df_weather)
-    df_weather_clean.to_csv("weather_cleaned.csv", index=False, encoding="utf-8")
-    print("Tabela weather_cleaned.csv gerada.")
 
 if __name__ == "__main__":
     main()
